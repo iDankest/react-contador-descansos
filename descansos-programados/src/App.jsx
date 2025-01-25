b0048d10740de4119875a6ffdd66526e0fe99944
iDankest <DankestMirror@hotmail.es>
2025-01-25 17:06:59 +0000

    principio, estructura HTML

--- descansos-programados/src/App.jsx
+++ descansos-programados/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='container'>
+      <h1>Descansos programados estudio o trabajo</h1>
+      <div className='container-inputs'>
+      <label htmlFor="">Tiempo de trabajo Obejtivo
+        <input type="number" name="" id="" />  
+      </label>
+      <label htmlFor="">Tiempo de descanso
+        <input type="number" name="" id="" />
+      </label>
+      <label htmlFor="">Frecuencia de los descanso
+        <input type="number" name="" id="" />
+      </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
-
 export default App
